7ce994fe16ceb12ac75fb79b3eadba3acae47b98
Rohit Raj <144614413+gh-rohit@users.noreply.github.com>
2025-06-02 18:48:03 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ require('dotenv').config(); // Load environment variables
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-   origin: "http://localhost:5173", // React frontend URL
+   origin: "https://youtube-clone-frontend-c8kj.onrender.com", // React frontend URL
    credentials: true,
    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Authorization"]
 }));
